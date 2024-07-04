65e470b52df558e5ecbc069e2f94004b78d61b27
Francesca Venditti <135988334+fnv234@users.noreply.github.com>
2024-07-04 12:28:42 -0400

    Added shading/coloring of the faces of the object

--- object.py
+++ object.py
@@ -10,6 +10,7 @@ class Object3D:
         self.color_faces = [(pg.Color('dodgerblue'), face) for face in self.faces]
         self.movement_flag, self.draw_vertices = True, True
         self.label = ''
+        self.light = np.array([0, 0, 1])  # Light direction
 
 
     def draw(self):
@@ -47,7 +48,12 @@ class Object3D:
             color, face = color_face
             polygon = vertices[face]
             if not np.any((polygon == self.render.H_WIDTH) | (polygon == self.render.H_HEIGHT)):
-                pg.draw.polygon(self.render.screen, color, polygon, 1)
+                normal = self.calculate_normal(face)
+                intensity = np.dot(normal, self.light)
+                shade_color = self.shade_color(color, intensity)
+                pg.draw.polygon(self.render.screen, shade_color, polygon, 0)  # fill the polygon
+                pg.draw.polygon(self.render.screen, pg.Color('black'), polygon, 1)  # draw outline
+
                 if self.label:
                     text = self.font.render(self.label[index], True, pg.Color('white'))
                     self.render.screen.blit(text, polygon[-1])
@@ -57,6 +63,28 @@ class Object3D:
                 if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
                     pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 2)
 
+    # calculate the normal vector of a face
+    def calculate_normal(self, face):
+        v1 = self.vertices[face[1]] - self.vertices[face[0]]
+        v2 = self.vertices[face[2]] - self.vertices[face[0]]
+        normal = np.cross(v1[:3], v2[:3])
+        normal = normal / np.linalg.norm(normal)
+        return normal
+
+    # shade the color of a face
+    def shade_color(self, color, intensity):
+        intensity = max(0, min(1, intensity))
+        shaded_color = (
+            min(255, max(0, int(color.r * intensity))),
+            min(255, max(0, int(color.g * intensity))),
+            min(255, max(0, int(color.b * intensity)))
+        )
+        return pg.Color(*shaded_color)
+    
+    def __str__(self):
+        return f'{self.__class__.__name__}'
+
+    
             
 class Axes(Object3D):
     def __init__(self, render):
